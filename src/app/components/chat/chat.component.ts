cd8719596a3ccb8a786b6b1822fd722c1de3491b
Siddharth Ajmera <Siddharth.Ajmera@T-Mobile.com>
2017-11-24 22:56:30 +0530

    Added speech recognition

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
-
+  recognition;
+  messages: string[] = [];
   constructor() { }
 
   ngOnInit() {
+    this.recognition = new (<any>window).webkitSpeechRecognition();
+  }
+
+  startRecognition() {
+    this.recognition.start();
+    this.recognition.onresult = event => { 
+      let userSaid = event.results[0][0].transcript;
+      this.messages.push(userSaid);
+    }
   }
 
 }
