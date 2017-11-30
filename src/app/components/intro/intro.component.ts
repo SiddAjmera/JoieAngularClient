19648ad561da3530d7a805ddd696634d52876d91
Siddharth Ajmera <Siddharth.Ajmera@T-Mobile.com>
2017-11-30 19:22:15 +0530

    Made some Content changes on intro

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -8,6 +8,24 @@ import { Router } from '@angular/router';
 })
 export class IntroComponent {
 
+  slideContent = [
+    {
+      imageUrl: 'https://mdbootstrap.com/img/Photos/Slides/img%20(130).jpg',
+      title: 'Hi! I\'m Joie',
+      description: 'Your very own mindfulness and mental wellbeing instructor.'
+    },
+    {
+      imageUrl: 'https://mdbootstrap.com/img/Photos/Slides/img%20(129).jpg',
+      title: 'I\'m smart',
+      description: 'I analyze your mood and suggest you activities to make you feel better.'
+    },
+    {
+      imageUrl: 'https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg',
+      title: 'I\'m accessible',
+      description: 'You can access me from your mobile devices, tablets as well as desktops.'
+    }
+  ];
+
   constructor(private _router: Router) { }
 
   navigateToChat() {
