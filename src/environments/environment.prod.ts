96957b017fb78befbe29a2fb9903f97e8406d9df
Jagdeep <js.soni@tcs.com>
2017-12-04 01:54:47 +0530

    removed keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,6 @@
 export const environment = {
   production: true,
   apiKeys: {
-    'webEmpath': '6zepqa2vrkXg4LLmogQ8tDm4BPwe4AbkIiL6aFQUsTw',
-    'dialogFlow': '4b734039daff49239773c05249b0b3b5',
-    'emotion': 'e3e810d7f010442198a0402aa6f2df8c',
-    'beyondVerbal': 'd9ac0db8-ff3e-4c8e-b8c7-b7cb1d89c964',
-    'youtubeAPIKey': 'AIzaSyCHFvJOYsOFnD7DYso-gl-AEYdZj1sCsmo',
-    'youtubeClientId': '953106500496-07qlbhh1hslbii32994hjb16qu08v3e7.apps.googleusercontent.com',
-    'youtubeClientSecret': 'sJvqCs6ARekJfzqt-X-8uj4-',
-    'spotifyClientId': '5b28b4684ac24afb9344303e6cd8b110',
-    'spotifyClientSecret': 'dd0a2efbb68e427cafb4e51d29f4690a'
+   
     }
 };
