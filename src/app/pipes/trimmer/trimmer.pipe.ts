48646aa00357d7970fe972f89bbba726b0c820f8
Siddharth Ajmera <Siddharth.Ajmera@T-Mobile.com>
2017-12-04 13:33:50 +0530

    Added styling for card.

--- src/app/pipes/trimmer/trimmer.pipe.ts
+++ src/app/pipes/trimmer/trimmer.pipe.ts
@@ -6,7 +6,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TrimmerPipe implements PipeTransform {
 
   transform(input: string, lengthToTrimTo?: any): string {
-    return input.trim().slice(0, lengthToTrimTo) + '...';
+    return input.trim().length > lengthToTrimTo ? input.trim().slice(0, lengthToTrimTo) + '...'
+                                                : input.trim();
   }
 
 }
